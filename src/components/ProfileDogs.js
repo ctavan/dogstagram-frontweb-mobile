3ac1f7821095c7e5dc7a5282134759328b649ee5
“Chinomnso <coawazie@gmail.com>
2020-09-09 00:59:42 -0400

    Added no of comments and likes to each dog picture

--- src/components/ProfileDogs.js
+++ src/components/ProfileDogs.js
@@ -9,22 +9,17 @@ import {
   Text,
 } from 'react-native';
 
-import Loader from '../animations/Loader';
-
 const ProfileDogs = (props) => {
   const {items} = props;
   const itemSize = (Dimensions.get('window').width - 12) / 3;
-  const [total, setTotal] = useState(props.items.length);
-
   const extractItemKey = (index) => `${index}`;
 
   console.log(items);
 
   const renderItem = ({item, index}) => (
-      <React.Fragment>
-      <TouchableOpacity onPress={() => alert('add functionality to open')}>
+    <React.Fragment>
+      <TouchableOpacity onPress={() => alert('This is the action that will lead to personal feed of users dog images')}>
         <Image
-      
           style={{
             width: itemSize,
             height: itemSize,
@@ -32,6 +27,23 @@ const ProfileDogs = (props) => {
           }}
           source={item.photo}
         />
+        <View style={styles.dogInfo}>
+          <View style={styles.section}>
+            <Text style={styles.space}>{Object.keys(item.likes).length}</Text>
+            <Text style={styles.space2}>likes</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.space}>
+              {Object.keys(item.comments).length}
+            </Text>
+            <Text
+              style={styles.space2}
+              onPress={() => alert('this is where comments live')}>
+              comments
+            </Text>
+          </View>
+        </View>
+        {/**add dog detials like comments, likes and comment replies here */}
       </TouchableOpacity>
     </React.Fragment>
   );
@@ -48,16 +60,33 @@ const ProfileDogs = (props) => {
   );
 };
 
-  const styles = StyleSheet.create({
-      images: {
-          flexDirection: 'row',
-          paddingHorizontal: 0.5,
-      },
-      header: {
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          backgroundColor: 'white',
-      },
-  });
+const styles = StyleSheet.create({
+  images: {
+    flexDirection: 'row',
+    paddingHorizontal: 0.5,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+  },
+  section: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginHorizontal: '5%',
+  },
+  space: {
+    color: '#151a30',
+    fontWeight: 'bold',
+  },
+  space2: {
+    color: '#aab9b7',
+    fontWeight: 'bold',
+  },
+  dogInfo: {
+    flex: 1,
+    paddingVertical: 10,
+  },
+});
 
 export default ProfileDogs;
